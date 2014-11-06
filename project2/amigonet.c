59595e70b5d62481b217d35283c64a32347a35e6
bigshebang <ljm3103@rit.edu>
2014-11-05 22:26:59 -0500

    add initial files

---
+++ project2/amigonet.c
@@ -0,0 +1,9 @@
+/*  ljm3103 - Luke Matarazzo
+ *  CSCI-243 -- Project 2 - Amigonet
+ *  filename: amigonet.c
+ *  File containing TO DO: complete this
+ */
+
+#include <stdlib.h>
+#include "amigonet.h"
+
