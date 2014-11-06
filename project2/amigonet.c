c346bb01dd53c579c6c9256c1c71bbd7fd0aa785
bigshebang <ljm3103@rit.edu>
2014-11-06 00:34:49 -0500

    git diff

--- project2/amigonet.c
+++ project2/amigonet.c
@@ -7,3 +7,43 @@
 #include <stdlib.h>
 #include "amigonet.h"
 
+void create_amigonet()
+{
+	//code
+}
+
+void destroy_amigonet()
+{
+	//code
+}
+
+void addUser(const char *name)
+{
+	//code
+}
+
+User *findUser(const char *name)
+{
+	//code
+}
+
+void addAmigo(User *user, User *amigo)
+{
+	//code
+}
+
+void removeAmigo(User *user, User *ex_amigo)
+{
+	//code
+}
+
+size_t separation(const User *user1, const User *user2)
+{
+	//code
+}
+
+void dump_date()
+{
+	//code
+}
+
