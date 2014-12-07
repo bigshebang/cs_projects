7b1396b99a5207e5ee6839eb40036ddb44f06513
Luke Matarazzo <ljm3103@rit.edu>
2014-12-07 00:05:07 -0500

    move some code around, implement getline

--- project3/mysh.c
+++ project3/mysh.c
@@ -1,25 +1,24 @@
 /*  ljm3103 - Luke Matarazzo
  *  CSCI-243 -- Project 3 - MY SHell
  *  filename: mysh.c
- *	DESCRIPTION
+ *	Main function for my shell (MYSH).
  */
+
+#define _GNU_SOURCE
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+
+#include "builtin.h"
 
 //return codes
 #define EXIT_INVALID_ARG	-1
 
 //global variables
 static short verboseMode = 0;
-static unsigned long commandHistory = 10;
-static const char USAGE_MESS[] = "usage: mysh [-v] [-h pos_num]";
 
-//path to search for binaries/commands in
-static const char path [] = "/usr/local/dcs/jdk/bin:/usr/local/dcs/bin:"
-							"/usr/local/sbin:/usr/local/bin:/usr/sbin:"
-							"/usr/bin:/sbin:/bin:/usr/games:";
 
 int main(int argc, char * argv[])
 {
@@ -53,8 +52,29 @@ int main(int argc, char * argv[])
 		}
 	}
 
-	printf("verboseMode is %d\n", verboseMode);
-	printf("commandHistory is %ld\n", commandHistory);
+	//initialize all necessary variables
+	static unsigned long commandHistory = 10;
+	static unsigned long curCommand = 1;
+	static const char PROMPT[] = "mysh[%s]> ";
+	static const char USAGE_MESS[] = "usage: mysh [-v] [-h pos_num]";
+
+	//path to search for binaries/commands in
+	static const char path[] = 	"/usr/local/dcs/jdk/bin:/usr/local/dcs/bin:"
+								"/usr/local/sbin:/usr/local/bin:/usr/sbin:"
+								"/usr/bin:/sbin:/bin:/usr/games:";
+
+	char *prevCommands[commandHistory]; //command history array
+
+	char *inputBuf = NULL;
+
+	//get line while we have actually read bytes
+	while((size_t ret = getline(&inputBuf, 0, stdin)) >= 0)
+	{
+		if(strcmp(inputBuf, EOF) == 0 || strcmp(inputBuf, "quit"))
+			break;
+
+		printf("you entered '%s'", inputBuf);
+	}
+
 	return EXIT_SUCCESS;
 }
-
