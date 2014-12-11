a3331094b476455d9df72f6e9dfdc4c710414c90
bigshebang <ljm3103@rit.edu>
2014-12-11 18:06:09 -0500

    fix comments

--- project3/external.h
+++ project3/external.h
@@ -1,11 +1,12 @@
 /*  ljm3103 - Luke Matarazzo
  *  CSCI-243 -- Project 3 - MY SHell
  *  filename: external.h
- *	Header file for the external command handling/running module
+ *	Header file for the external command handling/running module.
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
+//fork/exec the given command with its arguments
 int run(char **args);
