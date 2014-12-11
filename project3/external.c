4cc43f7bff59295e9710c374e9a9e987388685ff
Luke Matarazzo <ljm3103@rit.edu>
2014-12-11 17:03:54 -0500

    fix fork/execing

--- project3/external.c
+++ project3/external.c
@@ -1,27 +1,28 @@
 /*  ljm3103 - Luke Matarazzo
  *  CSCI-243 -- Project 3 - MY SHell
  *  filename: history.c
- *	Implementation of all the command execution/running/handling module.
+ *	Implementation of the command execution/running/handling module.
  */
 
 #include <unistd.h>
 #include <sys/wait.h>
 #include "external.h"
 
+#define	EXIT_WAIT_FAIL	-1
+
 /*  Function: run
  *  Parameters: args - array of strings containing the arguments
- *  Purpose: Initialize a command history array to have all null pointers.
- *  Returns: The return status of the program.
+ *  Purpose: Fork then exec and run given command with args.
+ *  Returns: The return status of the program. -1 if waiting fails.
  */
 int run(char **args)
 {
+	//fork; if child, exec; if parent wait and return child's exit status
 	pid_t pid;
-	//fork, execvp(args[0], args)
-	//may need to add NULL to the end of args or something?
-	// ^ did that in split function, should be good but should be tested
 	if((pid = fork()) == -1) //if error
 	{
-		//code
+		perror("mysh");
+		return pid;
 	}
 	else if(!pid) //if child
 	{
@@ -32,20 +33,20 @@ int run(char **args)
 	}
 	else //if parent
 	{
-		//******************************************************
-		//		    not sure if this is done..check it!
-		//******************************************************
 		//wait for child to finish, read exit status and return
 		int status = 0;
 		pid_t ret = wait(&status);
-		if(ret == -1)
-			perror("mysh");
 
-		//check exit status of child
-		if(WIFEXITED(status))
+		if(ret == -1) //if error waiting
 		{
-			int exit_status = WEXITSTATUS(status);
-			return exit_status;
+			perror("mysh");
+			return ret;
 		}
+
+		//check exit status of child and return if it's finished
+		if(WIFEXITED(status))
+			return WEXITSTATUS(status);
+
+		return EXIT_WAIT_FAIL;
 	}
 }
