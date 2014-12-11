19e89d9e89bfa1965a5116da6df04b1c2bdc34ff
bigshebang <ljm3103@rit.edu>
2014-12-11 17:52:59 -0500

    make child return -2 if failure so we know in main

--- project3/external.c
+++ project3/external.c
@@ -6,14 +6,17 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include "external.h"
 
 #define	EXIT_WAIT_FAIL	-1
+#define EXIT_CHILD_FAIL	-2
 
 /*  Function: run
  *  Parameters: args - array of strings containing the arguments
  *  Purpose: Fork then exec and run given command with args.
- *  Returns: The return status of the program. -1 if waiting fails.
+ *  Returns: The return status of the program. -1 if waiting fails, -2 if the
+	child fails during the exec.
  */
 int run(char **args)
 {
@@ -29,13 +32,13 @@ int run(char **args)
 		int ret = execvp(args[0], args); //execute given command with args
 		if(ret == -1) //if problem with exec
 			perror("mysh");
-		return ret;
+		return EXIT_CHILD_FAIL;
 	}
 	else //if parent
 	{
 		//wait for child to finish, read exit status and return
 		int status = 0;
-		pid_t ret = wait(&status);
+		pid_t ret = waitpid(pid, &status, 0);
 
 		if(ret == -1) //if error waiting
 		{
