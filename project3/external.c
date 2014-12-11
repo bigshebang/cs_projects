8b8e160521ae3acece7a3ce68e1f9c0e5ae520c2
Luke Matarazzo <ljm3103@rit.edu>
2014-12-11 00:18:24 -0500

    adding external module

---
+++ project3/external.c
@@ -0,0 +1,10 @@
+/*  ljm3103 - Luke Matarazzo
+ *  CSCI-243 -- Project 3 - MY SHell
+ *  filename: history.c
+ *	Implementation of all functions related to managing and using command
+ 	history.
+ */
+
+#include "external.h"
+
+
---
+++ project3/external.h
@@ -0,0 +1,10 @@
+/*  ljm3103 - Luke Matarazzo
+ *  CSCI-243 -- Project 3 - MY SHell
+ *  filename: history.h
+ *	Header file for command history module. Circle buffer is used to store the
+ 	command history.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
