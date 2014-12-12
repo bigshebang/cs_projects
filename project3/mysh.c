b0efdd941a42af329f99a19dac8b68a43ad98721
Luke Matarazzo <ljm3103@rit.edu>
2014-12-11 21:19:46 -0500

    fix log error for command status print

--- project3/mysh.c
+++ project3/mysh.c
@@ -150,7 +150,7 @@ int main(int argc, char * argv[])
 				progRet = runRet;
 				break;
 			}
-			if(!runRet || verboseMode)
+			if(runRet || verboseMode)
 				printf("Command status: %d\n", runRet);
 		}
 
